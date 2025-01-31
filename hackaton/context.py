c5c9b73a9117ed9ae00924c582221c74bd7e9a16
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-01-31 16:14:10 +0100

    Added get context function
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

---
+++ hackaton/context.py
@@ -0,0 +1,48 @@
+import logging as log
+
+def get_context(args):
+    ctx = { "any": True }
+
+    if args.build_type == "debug":
+        log.debug('Setting context "debug" to True')
+        ctx["debug"] = True
+        log.debug('Setting context "release" to False')
+        ctx["release"] = False
+    elif args.build_type == "release":
+        log.debug('Setting context "debug" to False')
+        ctx["debug"] = False
+        log.debug('Setting context "release" to True')
+        ctx["release"] = True
+    else:
+        assert False, f"Illegal build type argument (--build-type={args.build_type})"
+    assert ctx["release"] != ctx["debug"], 'Context cannot have both "debug" and "release"'
+
+    if args.project == "community":
+        log.debug('Setting context "community" to True')
+        ctx["community"] = True
+        log.debug('Setting context "enterprise" to False')
+        ctx["enterprise"] = False
+    elif args.project == "enterprise":
+        log.debug('Setting context "community" to False')
+        ctx["community"] = False
+        log.debug('Setting context "enterprise" to True')
+        ctx["enterprise"] = True
+    else:
+        assert False, f"Illegal project argument (--project={args.project})"
+    assert ctx["community"] != ctx["enterprise"], 'Context cannot have both "community" and "enterprise"'
+
+    if args.role == "agent":
+        log.debug('Setting context "agent" to True')
+        ctx["agent"] = True
+        log.debug('Setting context "hub" to False')
+        ctx["hub"] = False
+    elif args.role == "hub":
+        log.debug('Setting context "agent" to False')
+        ctx["agent"] = False
+        log.debug('Setting context "hub" to True')
+        ctx["hub"] = True
+    else:
+        assert False, f"Illegal role argument (--role={args.role})"
+    assert ctx["agent"] != ctx["hub"], 'Context cannot have both "agent" and "hub"'
+
+    return ctx
--- hackaton/main.py
+++ hackaton/main.py
@@ -1,9 +1,11 @@
 import logging as log
 from args import parse_args
+from context import get_context
 
 
 def main():
     args = parse_args()
     loglevel = "DEBUG" if args.debug else "INFO"
     log.basicConfig(format="[%(filename)s:%(lineno)d][%(levelname)s]: %(message)s", level=loglevel)
-    log.debug("Test")
+
+    ctx = get_context(args)
