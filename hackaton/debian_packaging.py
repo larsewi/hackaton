478d5a06a1ba0cd5999cf2d062568eebdebb7d29
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-02-07 16:38:26 +0100

    Create debian contol and copyright files
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- hackaton/debian_packaging.py
+++ hackaton/debian_packaging.py
@@ -5,8 +5,9 @@ from datetime import datetime as dt
 from hackaton.utils import get_sources_dir
 
 
-def get_target_dir(pkg_name, pkg_version):
+def get_target_dir(pkg_name: str, pkg_version: str):
     sources_dir = get_sources_dir()
+    pkg_name = f"cfbuild-{pkg_name.lower()}"
     return os.path.join(sources_dir, f"{pkg_name}_{pkg_version}")
 
 
@@ -27,7 +28,7 @@ def clean_debian_dir(debian_dir):
             os.rmdir(path)
 
 
-def create_debian_changelog(pkg_name, pkg_version):
+def create_debian_changelog(pkg_name: str, pkg_version: str):
     """This is the log of changes to the Debian package. It does not need to
     list everything that has changed in upstream code, but a summary is helpful
     for others. We will not log anything, because we are lazy. However, we still
@@ -41,15 +42,99 @@ def create_debian_changelog(pkg_name, pkg_version):
     now = dt.now().astimezone().strftime("%a, %d %b %Y %H:%M:%S %z")
 
     with open(changelog, "w") as f:
-        print(f"{pkg_name} ({pkg_version}-1) UNRELEASED; urgency=low", file=f)
+        print(f"cfbuild-{pkg_name.lower()} ({pkg_version}-1) UNRELEASED; urgency=low", file=f)
         print("\n", file=f)  # Two newlines
-        print(f" -- CFEngine <cfengine@northern.tech>  {now}", file=f)
+        print(f" -- CFEngine Packager <cfengine@northern.tech>  {now}", file=f)
 
-    log.info(f"Created changelog '{changelog}'")
+    log.info(f"Created changelog file '{changelog}'")
 
     # The above should produce the same output as the following command.
     f"debchange --create --package {pkg_name} --newversion {pkg_version}-1 --urgency low --empty"
     # Check out `man debchange`` for more info.
+
+def create_debian_control(pkg_name: str, pkg_version: str):
+    """The control file describes the source and binary package, and gives some
+    information about them, such as their names, who the package maintainer is,
+    and so on."""
+
+    target_dir = get_target_dir(pkg_name, pkg_version)
+    debian_dir = get_debian_dir(target_dir)
+    control = os.path.join(debian_dir, "control")
+
+    with open(control, "w") as f:
+        #########################################
+        # Source package stanza
+        #########################################
+
+        # The source package name
+        print(f"Source: cfbuild-{pkg_name.lower()}", file=f)
+        print("Section: libs", file=f)
+        # The priority of the package (one of 'required', 'important',
+        # 'standard' or 'optional'). In general, a package is 'optional' unless
+        # it's 'essential' for a standard functioning system, i.e., booting or
+        # networking functionality.
+        print("Priority: optional", file=f)
+        # The name and e-mail address of the person responsible for the package.
+        # We will put the blame on Mr. CFEngine Packager.
+        print("Maintainer: CFEngine Packager <cfengine@northern.tech>", file=f)
+        print("Standards-Version: 4.7.0", file=f)
+        # The list of packages that need to be installed to build the package.
+        # They might or might not be needed to actually use the package.
+        print("Build-Depends: debhelper-compat (= 13)", file=f)
+
+        print(file=f) # Double newline
+
+        #########################################
+        # Binary package stanza
+        #########################################
+
+        # The name of the binary package. The name might be different from the
+        # source package name.
+        print(f"Package: cfbuild-{pkg_name.lower()}", file=f)
+        print("Section: libs", file=f)
+        # Specifies which computer architectures the binary package is expected
+        # to work on. "any" means that the package can be built for any
+        # architecture. "all" means that the same package will work on all
+        # architectures. For example, a package consisting only of shell scripts
+        # would be "all".
+        print("Architecture: any", file=f)
+        # The full description of the binary package. It is meant to be helpful
+        # to users. The first line is used as the short synopsis (summary)
+        # description, and the rest of the description must be an independent
+        # longer description of the package.
+        print(f"Description: CFEngine Build Automation -- {pkg_name}", file=f)
+        print(f" CFEngine Build Automation -- {pkg_name}", file=f)
+
+        print(file=f) # Double newline
+
+        #########################################
+        # Developer binary package stanza
+        #########################################
+
+        print(f"Package: cfbuild-{pkg_name.lower()}-devel", file=f)
+        print("Section: libdevel", file=f)
+        print("Architecture: any", file=f)
+        print(f"Description: CFEngine Build Automation -- {pkg_name} -- development files", file=f)
+        print(f" CFEngine Build Automation -- {pkg_name} -- development files", file=f)
+
+    log.info(f"Created control file '{control}'")
+
+def create_debian_copyright(pkg_name: str, pkg_version: str):
+    target_dir = get_target_dir(pkg_name, pkg_version)
+    debian_dir = get_debian_dir(target_dir)
+    copyright = os.path.join(debian_dir, "copyright")
+
+    # It is quite an important file, but for now we will be happy enough with an
+    # empty file.
+    with open(copyright, "w"):
+        pass
+
+    log.info(f"Created copyright file '{copyright}'")
+
+def create_debian_rules(pkg_name, pkg_version):
+    target_dir = get_target_dir(pkg_name, pkg_version)
+    debian_dir = get_debian_dir(target_dir)
+    rules = os.path.join(debian_dir, "rules")
 
 
 def prepare_debian_dir(pkg_name, pkg_version):
@@ -63,3 +148,5 @@ def prepare_debian_dir(pkg_name, pkg_version):
         os.makedirs(debian_dir, exist_ok=True)
 
     create_debian_changelog(pkg_name, pkg_version)
+    create_debian_control(pkg_name, pkg_version)
+    create_debian_copyright(pkg_name, pkg_version)
